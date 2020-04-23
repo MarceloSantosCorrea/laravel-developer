1577850867b9634a8df6c90af22b90d0d6aa4ab5
Marcelo Corrêa <marcelocorrea229@gmail.com>
2020-04-22 23:55:11 -0300

    ....

--- app/Models/AclRoleUser.php
+++ app/Models/AclRoleUser.php
@@ -9,4 +9,78 @@ class AclRoleUser extends Model
     protected $table      = 'acl_role_user';
     public    $timestamps = false;
     protected $fillable   = ['user_id', 'acl_role_id'];
+
+    /**
+     * Create Custom
+     *
+     * @param  array  $data
+     * @return User|\Illuminate\Database\Eloquent\Model
+     * @throws \Exception
+     */
+    public static function createCustom(Array $data)
+    {
+        try {
+            \DB::beginTransaction();
+
+            if ($model = self::create($data)) {
+                \DB::commit();
+
+                return $model;
+            }
+        } catch (\Exception $e) {
+            \DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Update Custom
+     *
+     * @param  array  $data
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return \Illuminate\Database\Eloquent\Model
+     * @throws \Exception
+     */
+    public static function updateCustom(Array $data, \Illuminate\Database\Eloquent\Model $model)
+    {
+        try {
+            \DB::beginTransaction();
+
+            $model->fill($data);
+
+            if ($model->update()) {
+                \DB::commit();
+
+                return $model;
+            }
+
+        } catch (\Exception $e) {
+            \DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Delete Custom
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $user
+     * @return \Illuminate\Database\Eloquent\Model
+     * @throws \Exception
+     */
+    public static function deleteCustom(\Illuminate\Database\Eloquent\Model $user)
+    {
+        try {
+            \DB::beginTransaction();
+
+            if ($user->delete()) {
+                \DB::commit();
+
+                return $user;
+            }
+
+        } catch (\Exception $e) {
+            \DB::rollBack();
+            throw $e;
+        }
+    }
 }
